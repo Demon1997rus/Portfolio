9929874dc11d4b09a45ff38d608b4f6796a2e048
Demon1997rus <rotanov.1997@mail.ru>
2023-08-06 07:24:52 -0700

    update functional List

--- library/list.h
+++ library/list.h
@@ -54,13 +54,34 @@ private:
 public:
     List()
     {
-        this->size = 0;
+        size = 0;
         head = tail = nullptr;
+    }
+
+    List(int count, const T& value) : List()
+    {
+        for (int i = 0; i < count; ++i)
+        {
+            push_back(value);
+        }
     }
 
     int count() const
     {
         return size;
+    }
+
+    int count(const T& value) const
+    {
+        int count = 0;
+        for (Node* current = head; current; current = current->next)
+        {
+            if (current->data == value)
+            {
+                ++count;
+            }
+        }
+        return count;
     }
 
     void push_back(T value)
@@ -91,14 +112,80 @@ public:
         }
     }
 
-    T& operator[](const int& index)
+    T& operator[](int index)
     {
         return findNode(index)->data;
     }
 
-    T at(const int& index) const
+    T at(int index) const
     {
         return findNode(index)->data;
+    }
+
+    void pop_front()
+    {
+        if (!head)
+            return;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        temp = nullptr;
+        --size;
+    }
+
+    void pop_back()
+    {
+        if (!tail)
+            return;
+        Node* temp = tail;
+        tail = tail->prev;
+        delete temp;
+        temp = nullptr;
+        --size;
+    }
+
+    void clear()
+    {
+        while (size)
+            pop_front();
+    }
+
+    void insert(int index, const T& value)
+    {
+        if (index <= 0)
+        {
+            push_front(value);
+        }
+        else if (index >= size)
+        {
+            push_back(value);
+        }
+        else
+        {
+            Node* current = findNode(index);
+            Node* prev = current->prev;
+            Node* node = new Node(value, current, prev);
+            current->prev = node;
+            prev->next = node;
+            ++size;
+        }
+    }
+
+    bool contains(const T& value) const
+    {
+        for (Node* current = head; current; current = current->next)
+        {
+            if (current->data == value)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool empty() const
+    {
+        return size == 0;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include <chrono>
+
 #include "library/list.h"
 
 using namespace std;
 using namespace Containers;
 
+void printList(const List<int>& test)
+{
+    for (int i = 0; i < test.count(); ++i)
+    {
+        cout << test.at(i) << endl;
+    }
+    cout << endl;
+}
+
 int main()
 {
-    List<int> test;
+    auto start = chrono::steady_clock::now();
+    // testContainers
+    auto finish = chrono::steady_clock::now();
+    auto duration = finish - start;
+    auto total = chrono::duration_cast<chrono::milliseconds>(duration).count();
+    cout << "Total milliseconds: " << total << endl;
     return 0;
 }
