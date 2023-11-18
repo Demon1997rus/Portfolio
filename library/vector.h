2df6d3f123afc86546bb7cf441e0da1209e803c9
Demon1997rus <rotanov.1997@mail.ru>
2023-11-18 10:57:16 -0800

    Добавил все конструкторы в кастомный класс вектора, за исключением initializer list + и методы begin, end, size

--- library/vector.h
+++ library/vector.h
@@ -3,67 +3,97 @@
 #include <iosfwd>
 #include <bits/c++config.h>
 #include <bits/allocator.h>
-#include <initializer_list>
+#include <memory>
 
 namespace rdi
 {
 /*!
- * \brief The Vector class - кастомный класс вектора
+ * \brief The Vector class - кастомный класс вектора с аллокатором
  */
 template <class T, class Alloc = std::allocator<T>>
 class Vector
 {
     typedef T value_type;
+    typedef Alloc alloc_type;
     typedef std::size_t size_type;
     typedef value_type* iterator;
     typedef const value_type* const_iterator;
     typedef value_type& reference;
+    typedef const value_type& const_reference;
     typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+
+    void create_storage(size_type __n)
+    {
+        m_start = m_alloc.allocate(__n);
+        m_finish = m_start;
+        m_storage = m_start + __n;
+    }
+
+    void default_initialize(size_type __n) { m_finish = std::__uninitialized_default_n_a(m_start, __n, m_alloc); }
+
+    void fill_initialize(size_type __n, const value_type& __value)
+    {
+        m_finish = std::__uninitialized_fill_n_a(m_start, __n, __value, m_alloc);
+    }
+
+    void null_initialize(Vector<value_type, alloc_type>& rhs) noexcept
+    {
+        rhs.m_start = nullptr;
+        rhs.m_finish = nullptr;
+        rhs.m_storage = nullptr;
+    }
 
 public:
-    Vector() : m_used(0), m_capacity(0), m_data(nullptr) {}
+    Vector() noexcept : m_start(), m_finish(), m_storage() {}
 
-    Vector(size_type size) : m_used(size), m_capacity(size)
+    Vector(size_type size) : Vector()
     {
-        m_data = m_allocator.allocate(size);
-        for (size_type i = 0; i < size; ++i)
-        {
-            m_allocator.construct(m_data + i);
-        }
+        create_storage(size);
+        default_initialize(size);
     }
 
-    Vector(size_type size, const T& value) : m_used(size), m_capacity(size)
+    Vector(size_type size, const value_type& value) : Vector()
     {
-        m_data = m_allocator.allocate(size);
-        for (size_type i = 0; i < size; ++i)
-        {
-            m_allocator.construct(m_data + i);
-            m_data[i] = value;
-        }
+        create_storage(size);
+        fill_initialize(size, value);
     }
 
-    ~Vector()
+    Vector(const Vector<value_type, alloc_type>& other) : m_start(), m_finish(), m_storage(), m_alloc(other.m_alloc)
     {
-        for (size_type i = 0; i < m_used; ++i)
-        {
-            m_allocator.destroy(m_data + i);
-        }
-        m_allocator.deallocate(m_data, m_capacity);
+        create_storage(other.size());
+        default_initialize(other.size());
+        m_finish = std::__uninitialized_copy_a(other.begin(), other.end(), m_start, m_alloc);
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Vector<T>& other)
+    Vector(Vector<value_type, alloc_type>&& other) noexcept
+      : m_start(other.m_start), m_finish(other.m_finish), m_storage(other.m_storage), m_alloc(std::move(other.m_alloc))
     {
-        for (size_type i = 0; i < other.m_used; ++i)
+        null_initialize(other);
+    }
+
+    ~Vector() noexcept { std::_Destroy(m_start, m_finish, m_alloc); }
+
+    iterator begin() noexcept { return m_start; }
+    const_iterator begin() const noexcept { return m_start; }
+    iterator end() noexcept { return m_finish; }
+    const_iterator end() const noexcept { return m_finish; }
+
+    size_type size() const noexcept { return size_type(m_finish - m_start); }
+
+    friend std::ostream& operator<<(std::ostream& os, const Vector<value_type, alloc_type>& other)
+    {
+        for (const value_type& value : other)
         {
-            os << other.m_data[i] << " ";
+            os << value << " ";
         }
         return os;
     }
 
 private:
-    size_type m_used;
-    size_type m_capacity;
-    Alloc m_allocator;
-    value_type* m_data;
+    pointer m_start;
+    pointer m_finish;
+    pointer m_storage;
+    alloc_type m_alloc;
 };
 }  // namespace rdi
--- main.cpp
+++ main.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
-#include <vector>
 
-#include "library/nonbinarypolishrecord.h"
+#include "library/vector.h"
 
-using namespace std;
-
-int main()
-{
-    vector<string> tests{ "(*(+3 6)(*1 2 3))", "(+(*5 3)(*5 3))", "(+6 3)", "(-6 3)",
-                          "(*(+(/6 3)(*2 8))(+2 4)(-4 2))" };
-
-    for (const string& test : tests)
-    {
-        cout << "Результат " << test << " = " << rdi::nonbinarypolishrecord(test) << endl;
-    }
-
-    return 0;
-}
+int main() { return 0; }
